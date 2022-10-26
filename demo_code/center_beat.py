eb9cabdebce753007163ce7b4faaabd738b807b0
wendtalexander <wendtalexander@protonmail.com>
2022-10-26 21:35:26 +0200

    trying to center_beat, zero crossing

--- demo_code/center_beat.py
+++ demo_code/center_beat.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import rlxnix as rlx 
 from IPython import embed
+from scipy.signal import find_peaks
+from scipy.interpolate import interp1d
 
-dset = rlx.Dataset("data/2021-11-11-af-invivo-1.nix")
+dset = rlx.Dataset("../data/2021-11-11-af-invivo-1.nix")
 
 chirp1 = dset["Chirps_8"]
 frq_dset = dset.metadata["Recording"]["Subject"]["EOD Frequency"][0][0]
@@ -21,7 +23,22 @@ dt = stim_time[1]-stim_time[0]
 t_time = np.arange(0, t*multiple_ts, dt)
 t_times = range(len(t_time))
 
+
+zeros = np.where(np.diff(np.sign(stim)))[0]
+maxima_index = []
+for i in range(len(zeros)):
+    #maxima = np.argmax(np.arange(stim[zeros[i]], stim[zeros[i+2]], dt))
+    max_range = np.arange(stim_time[zeros[i]], stim_time[zeros[i+2]],dt)
+    max_index = range(len(max_range))
+    maximum = np.argmax(stim[max_index])
+    maxima_index.append(maximum)
+
+embed()
+exit()
+
+
 plt.plot(stim_time[t_times], stim[t_times], alpha=0.6)
 for i in range(multiple_ts):
     plt.scatter(t*i, np.zeros(1)*0.09)
 plt.show()
+
