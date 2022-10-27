714f4ffd816b81dfc817b9f74a189bdff59212bb
weygoldt <88969563+weygoldt@users.noreply.github.com>
2022-10-27 14:12:57 +0200

    working population simulation

--- demo_code/centerchirp.py
+++ demo_code/centerchirp.py
@@ -1,5 +1,3 @@
-from statistics import median
-
 import matplotlib.pyplot as plt
 import numpy as np
 import rlxnix as rlx
@@ -7,126 +5,208 @@ import rlxnix as rlx
 import functions as fs
 from termcolors import TermColor as tc
 
+
+def singlecell_cts(data):
+    """
+    singlecell_cts extracts the spikes centered around the chirp stimulus
+    (chirp-triggered spikes) onset for every single chirp stimulus.
+
+    Parameters
+    ----------
+    data : rlxnix dataset
+        Relacs dataset imported with rlxnix
+
+    Returns
+    -------
+    spike_t : array of arrays
+        an array including an array for every ct-spiketrain
+    c_time : array
+        the time centered around the chirp
+    """
+
+    # collect chirp-centered spike times here
+    spike_t = []
+
+    # padding around chirp
+    before_t = 0.1
+    after_t = 0.2
+
+    # find all chirp repros
+    chirp_repros = [i for i in data.repros if "Chirps" in i]
+
+    # go through all chirp repros
+    for repro in chirp_repros:
+
+        # get chirps from each repro
+        chirps = d[repro]
+
+        for i in range(chirps.stimulus_count):
+
+            # get data
+            _, time = chirps.membrane_voltage(i)
+            spikes = chirps.spikes(i)
+            chirp_times = chirps.chirp_times[0][i]
+
+            # compute number of indices before and after chirp to include
+            dt = time[1] - time[0]
+            before_indices = np.round(before_t / dt)
+            after_indices = np.round(after_t / dt)
+
+            for c in chirp_times:
+
+                # where is chirp on time vector?
+                c_index = fs.find_closest(time, c)
+
+                # make index vector centered around chirp
+                indices = np.arange(
+                    c_index - before_indices, c_index + after_indices, dtype=int
+                )
+
+                # get max t and min t
+                try:
+                    c_time = time[indices]
+                except:
+                    print(tc.warn(f"Trial {i} Repro {repro} skipped, not enough data!"))
+                    print(f"max index: {np.max(indices)}")
+                    print(f"time length: {len(time)}")
+                    continue
+
+                tmin = np.min(c_time)
+                tmax = np.max(c_time)
+
+                # get spike times in this range
+                c_spikes = spikes[(spikes > tmin) & (spikes < tmax)]
+
+                # get spike indices on c_time vector
+                c_spike_indices = [fs.find_closest(c_time, x) for x in c_spikes]
+
+                # make new centered time array
+                c_time = np.arange(-before_indices * dt, (after_indices + 1) * dt, dt)
+
+                # extract spike timestamps from centered time
+                c_spikes_centered = c_time[c_spike_indices]
+
+                # append centered spike times to list
+                spike_t.append(c_spikes_centered)
+
+    return spike_t, c_time
+
+
+def hompopulation_cts(data):
+    # collect chirp-centered spike times here
+    spike_t = []
+
+    # padding around chirp
+    before_t = 0.1
+    after_t = 0.2
+
+    # find all chirp repros
+    chirp_repros = [i for i in data.repros if "Chirps" in i]
+
+    # go through all chirp repros
+    for repro in chirp_repros:
+
+        # get chirps from each repro
+        chirps = d[repro]
+
+        for i in range(chirps.stimulus_count):
+
+            # get data
+            _, time = chirps.membrane_voltage(i)
+            spikes = chirps.spikes(i)
+            chirp_times = chirps.chirp_times[0][i]
+
+            # compute number of indices before and after chirp to include
+            dt = time[1] - time[0]
+            before_indices = np.round(before_t / dt)
+            after_indices = np.round(after_t / dt)
+
+            # collect spikes for this trial here
+            spikelist = []
+
+            for c in chirp_times:
+
+                # where is chirp on time vector?
+                c_index = fs.find_closest(time, c)
+
+                # make index vector centered around chirp
+                indices = np.arange(
+                    c_index - before_indices, c_index + after_indices, dtype=int
+                )
+
+                # get max t and min t
+                try:
+                    c_time = time[indices]
+                except:
+                    print(tc.warn(f"Trial {i} Repro {repro} skipped, not enough data!"))
+                    print(f"max index: {np.max(indices)}")
+                    print(f"time length: {len(time)}")
+                    continue
+
+                tmin = np.min(c_time)
+                tmax = np.max(c_time)
+
+                # get spike times in this range
+                c_spikes = spikes[(spikes > tmin) & (spikes < tmax)]
+
+                # get spike indices on c_time vector
+                c_spike_indices = [fs.find_closest(c_time, x) for x in c_spikes]
+
+                # make new centered time array
+                c_time = np.arange(-before_indices * dt, (after_indices + 1) * dt, dt)
+
+                # extract spike timestamps from centered time
+                c_spikes_centered = c_time[c_spike_indices]
+
+                # append centered spike times to list
+                spikelist.append(c_spikes_centered)
+
+            # flatten spike list to simulate activity of hom population
+            spikelist_flat = fs.flatten(spikelist)
+
+            # save to spike times list
+            spike_t.append(spikelist_flat)
+
+    return spike_t, c_time
+
+
 # get data
 d = rlx.Dataset("../data/data_2021/2021-11-11-af-invivo-1.nix")
 
-# collect chirp-centered spike times here
-spike_t = []
-
-# padding around chirp
-before_t = 0.1
-after_t = 0.2
-
-# find all chirp repros
-chirp_repros = [i for i in d.repros if "Chirps" in i]
-
-# go through all chirp repros
-for repro in chirp_repros:
-
-    # get chirps from each repro
-    chirps = d[repro]
-
-    for i in range(chirps.stimulus_count):
-
-        # get data
-        v, time = chirps.membrane_voltage(i)
-        spikes = chirps.spikes(i)
-        chirp_times = chirps.chirp_times[0][i]
-
-        # compute number of indices before and after chirp to include
-        dt = time[1] - time[0]
-        before_indices = np.round(before_t / dt)
-        after_indices = np.round(after_t / dt)
-
-        for c in chirp_times:
-
-            # where is chirp on time vector?
-            c_index = fs.find_closest(time, c)
-
-            # make index vector centered around chirp
-            indices = np.arange(
-                c_index - before_indices, c_index + after_indices + 1, dtype=int
-            )
-
-            # get max t and min t
-            try:
-                c_time = time[indices]
-                # print(f"max index: {np.max(indices)}")
-                # print(f"time length: {len(time)}")
-            except:
-                print(tc.err(f"Trial {i} Repro {repro} skipped, not enough data!"))
-                print(f"max index: {np.max(indices)}")
-                print(f"time length: {len(time)}")
-                continue
-
-            tmin = np.min(c_time)
-            tmax = np.max(c_time)
-
-            # get spike times in this range
-            c_spikes = spikes[(spikes > tmin) & (spikes < tmax)]
-
-            # get spike indices on c_time vector
-            c_spike_indices = [fs.find_closest(c_time, x) for x in c_spikes]
-
-            # make new centered time array
-            c_time = np.arange(-before_indices * dt, (after_indices + 1) * dt, dt)
-
-            # extract spike timestamps from centered time
-            c_spikes_centered = c_time[c_spike_indices]
-
-            # append centered spike times to list
-            spike_t.append(c_spikes_centered)
-
-
-# remove empty entries
-spike_t_cleaned = []
-for spiketrain in spike_t:
-    if len(spiketrain) == 0:
-        continue
-    else:
-        spike_t_cleaned.append(spiketrain)
+# extract centered spiketimes
+singlecell_spikes, time = singlecell_cts(d)
+hompopulation_spikes, time = hompopulation_cts(d)
 
 # compute gamma kde with coustom time array
 kdetime = np.linspace(
-    np.min(c_time),
-    np.max(c_time),
-    500,
+    np.min(time),
+    np.max(time),
+    1000,
 )
 
-# compute kdes
-kdes = []
-for spiketrain in spike_t_cleaned:
-    rate = fs.causal_kde1d(spikes=spiketrain, time=kdetime, width=0.002, shape=2)
-    kdes.append(rate)
+# compute kdes for every chirp-centered spiketrain
+singlecell_kdes = [fs.causal_kde1d(x, kdetime, 0.002) for x in singlecell_spikes]
+
+# compute kde for population activity
+hompopulation_kdes = [fs.causal_kde1d(x, kdetime, 0.002) for x in hompopulation_spikes]
 
 # compute mean kdes
-kdes = np.array(kdes)
-mean_kde = []
-median_kde = []
-for i in range(len(kdetime)):
-    colmean = np.mean(kdes[:, i])
-    colmedian = np.median(kdes[:, i])
-
-    colstd = np.std(kdes[:, i])
-    mean_kde.append(colmean)
-    median_kde.append(colmedian)
-
-baseline_q5s = np.quantile(kdes, 0.05, axis=0)
-baseline_q95s = np.quantile(kdes, 0.95, axis=0)
+# singlecell_kdes = np.array(singlecell_kdes)
+singlecell_mean = np.mean(singlecell_kdes, axis=0)
+hompopulation_mean = np.mean(hompopulation_kdes, axis=0)
+# chirpspikes_median = np.median(singlecell_kdes, axis=0)
+# chirpspikes_q5s = np.quantile(singlecell_kdes, 0.05, axis=0)
+# chirpspikes_q95s = np.quantile(singlecell_kdes, 0.95, axis=0)
 
 fig, ax = plt.subplots()
 ax.eventplot(
-    spike_t,
-    # lineoffsets=np.linspace(0, np.max(mean_kde), len(spike_t)),
-    # linelengths=np.ones_like(len(spike_t)) * 0.8,
-    lineoffsets=0.01,
-    linelengths=0.1,
+    hompopulation_spikes,
+    lineoffsets=0.3,
+    linelengths=1,
     colors="darkgrey",
     alpha=1,
 )
 
 ax.axvline(0, 0, 100, color="black", linestyle="dashed", lw=1)
-ax.plot(kdetime, mean_kde, color="red", lw=2)
-
-ax.set_xlim(-0.08, 0.18)
+ax.plot(kdetime, hompopulation_mean, color="green", lw=2)
 plt.show()
--- demo_code/functions.py
+++ demo_code/functions.py
@@ -486,21 +486,206 @@ def causal_kde1d(spikes, time, width, shape=2):
     return rate
 
 
-# sorting chirp Data 
+# Data access and sorting
+
+
+def singlecell_cts(data):
+    """
+    singlecell_cts extracts the spikes centered around the chirp stimulus
+    (chirp-triggered spikes) onset for every single chirp stimulus.
+
+    Parameters
+    ----------
+    data : rlxnix dataset
+        Relacs dataset imported with rlxnix
+
+    Returns
+    -------
+    spike_t : array of arrays
+        an array including an array for every ct-spiketrain
+    c_time : array
+        the time centered around the chirp
+    """
+
+    # collect chirp-centered spike times here
+    spike_t = []
+
+    # padding around chirp
+    before_t = 0.1
+    after_t = 0.2
+
+    # find all chirp repros
+    chirp_repros = [i for i in data.repros if "Chirps" in i]
+
+    # go through all chirp repros
+    for repro in chirp_repros:
+
+        # get chirps from each repro
+        chirps = d[repro]
+
+        for i in range(chirps.stimulus_count):
+
+            # get data
+            _, time = chirps.membrane_voltage(i)
+            spikes = chirps.spikes(i)
+            chirp_times = chirps.chirp_times[0][i]
+
+            # compute number of indices before and after chirp to include
+            dt = time[1] - time[0]
+            before_indices = np.round(before_t / dt)
+            after_indices = np.round(after_t / dt)
+
+            for c in chirp_times:
+
+                # where is chirp on time vector?
+                c_index = fs.find_closest(time, c)
+
+                # make index vector centered around chirp
+                indices = np.arange(
+                    c_index - before_indices, c_index + after_indices, dtype=int
+                )
+
+                # get max t and min t
+                try:
+                    c_time = time[indices]
+                except:
+                    print(tc.warn(f"Trial {i} Repro {repro} skipped, not enough data!"))
+                    print(f"max index: {np.max(indices)}")
+                    print(f"time length: {len(time)}")
+                    continue
+
+                tmin = np.min(c_time)
+                tmax = np.max(c_time)
+
+                # get spike times in this range
+                c_spikes = spikes[(spikes > tmin) & (spikes < tmax)]
+
+                # get spike indices on c_time vector
+                c_spike_indices = [fs.find_closest(c_time, x) for x in c_spikes]
+
+                # make new centered time array
+                c_time = np.arange(-before_indices * dt, (after_indices + 1) * dt, dt)
+
+                # extract spike timestamps from centered time
+                c_spikes_centered = c_time[c_spike_indices]
+
+                # append centered spike times to list
+                spike_t.append(c_spikes_centered)
+
+    return spike_t, c_time
+
+
+def hompopulation_cts(data):
+    """
+    hompopulation_cts extracts the spikes centered around the chirp stimulus
+    (chirp-triggered spikes) onset for every single chirp stimulus and groups
+    them per trial. This simulates a population response for a homogenous
+    population of this cell.
+
+    Parameters
+    ----------
+    data : rlxnix dataset
+        Relacs dataset imported with rlxnix
+
+    Returns
+    -------
+    spike_t : array of arrays
+        an array including an array for every ct-spiketrain
+    c_time : array
+        the time centered around the chirp
+    """
+
+    # collect chirp-centered spike times here
+    spike_t = []
+
+    # padding around chirp
+    before_t = 0.1
+    after_t = 0.2
+
+    # find all chirp repros
+    chirp_repros = [i for i in data.repros if "Chirps" in i]
+
+    # go through all chirp repros
+    for repro in chirp_repros:
+
+        # get chirps from each repro
+        chirps = d[repro]
+
+        for i in range(chirps.stimulus_count):
+
+            # get data
+            _, time = chirps.membrane_voltage(i)
+            spikes = chirps.spikes(i)
+            chirp_times = chirps.chirp_times[0][i]
+
+            # compute number of indices before and after chirp to include
+            dt = time[1] - time[0]
+            before_indices = np.round(before_t / dt)
+            after_indices = np.round(after_t / dt)
+
+            # collect spikes for this trial here
+            spikelist = []
+
+            for c in chirp_times:
+
+                # where is chirp on time vector?
+                c_index = fs.find_closest(time, c)
+
+                # make index vector centered around chirp
+                indices = np.arange(
+                    c_index - before_indices, c_index + after_indices, dtype=int
+                )
+
+                # get max t and min t
+                try:
+                    c_time = time[indices]
+                except:
+                    print(tc.warn(f"Trial {i} Repro {repro} skipped, not enough data!"))
+                    print(f"max index: {np.max(indices)}")
+                    print(f"time length: {len(time)}")
+                    continue
+
+                tmin = np.min(c_time)
+                tmax = np.max(c_time)
+
+                # get spike times in this range
+                c_spikes = spikes[(spikes > tmin) & (spikes < tmax)]
+
+                # get spike indices on c_time vector
+                c_spike_indices = [fs.find_closest(c_time, x) for x in c_spikes]
+
+                # make new centered time array
+                c_time = np.arange(-before_indices * dt, (after_indices + 1) * dt, dt)
+
+                # extract spike timestamps from centered time
+                c_spikes_centered = c_time[c_spike_indices]
+
+                # append centered spike times to list
+                spikelist.append(c_spikes_centered)
+
+            # flatten spike list to simulate activity of hom population
+            spikelist_flat = fs.flatten(spikelist)
+
+            # save to spike times list
+            spike_t.append(spikelist_flat)
+
+    return spike_t, c_time
+
+
 def sort_reodfs(data):
     """Sorting of the relative EODs of chirps data.
 
     Parameters
     ----------
-    data : rlx.Dataset nix file 
+    data : rlx.Dataset nix file
         Dataset with different EODs for chirp data
     Returns
     -------
     dic
-        Dictionary with the relative EODs as keys, Items are the name of the trace  
+        Dictionary with the relative EODs as keys, Items are the name of the trace
     """
     r_eodf = []
-    for chirp in data.repro_runs('Chirps'):
+    for chirp in data.repro_runs("Chirps"):
         r_eodf.append(chirp.relative_eodf)
 
     r_eodf_arr = np.array(r_eodf)
@@ -511,8 +696,8 @@ def sort_reodfs(data):
     for unique_r_eodf in r_eodf_arr_uniq:
         r_eodf_dict[f"{unique_r_eodf}"] = []
         for r in range(len(r_eodf)):
-            chirps = data.repro_runs('Chirps')[r]
+            chirps = data.repro_runs("Chirps")[r]
             if unique_r_eodf == r_eodf[r]:
                 r_eodf_dict[f"{unique_r_eodf}"].append(chirps.name)
-    
+
     return r_eodf_dict
