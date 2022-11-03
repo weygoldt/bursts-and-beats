bfefca83f6770db0aed6ff9434ef32039e124671
wendtalexander <wendtalexander@protonmail.com>
2022-11-03 10:09:03 +0100

    changing beat without chirip

--- code/plot_beat_without_chrip.py
+++ code/plot_beat_without_chrip.py
@@ -39,9 +39,9 @@ ax3.plot(
 ax1.set_xlim(0, 0.3)
 ax2.set_ylim(-2.1, 2.1)
 
-ax1.text( 0.0, 0.99, f"Chirping sender: {eodf_send} Hz",transform=ax1.transAxes )
+ax1.text( 0.0, 1.1, f"Sender: {eodf_send} Hz",transform=ax1.transAxes )
 ax2.text( 0.0, 0.99, f"Receiver: {eodf_rec} Hz", transform=ax2.transAxes)
-ax2.text(0.0, 0.99, f"Amplitude modulation: {eodf_rec-eodf_send} Hz", transform=ax3.transAxes)
+ax2.text(0.0, 1.0, f"Amplitude modulation: {eodf_rec-eodf_send} Hz", transform=ax3.transAxes)
 
 # remove axes
 
