c19f08c7299b7c7141e8a5e3a53d7b0cb4e7aa6b
wendtalexander <wendtalexander@protonmail.com>
2022-11-03 10:03:09 +0100

    adding plot_beat without chirps

--- code/plot_beat_without_chrip.py
+++ code/plot_beat_without_chrip.py
@@ -4,7 +4,7 @@ import numpy as np
 import functions as fs
 from plotstyle import PlotStyle
 
-PlotStyle()
+s = PlotStyle()
 
 # make chirping individual (sender)
 time, eod_send, _, _ = fs.create_chirp()
@@ -13,40 +13,46 @@ time, eod_send, _, _ = fs.create_chirp()
 eodf_rec = 500
 eodf_send = 480
 eod_rec = 1 * np.sin(2 * np.pi * eodf_rec * time)
-eod_send = 0.5 * np.sin(2 * np.pi * eodf_send * time)
+eod_send = 0.2 * np.sin(2 * np.pi * eodf_send * time)
 # make envelope
 beat, envelope, envelope_time = fs.beat_envelope(eod_send, eod_rec, 500, eodf_rec, time)
 
 # plot
-fig, ax = plt.subplots(
+fig, (ax1, ax2, ax3) = plt.subplots(
     3,
     1,
     sharex=True,
     figsize=(16 * s.cm, 12 * s.cm),
     gridspec_kw={"height_ratios": [1, 1, 2]},
 )
-ax[0].plot(time, eod_send, lw=1, c="k")
-ax[1].plot(time, eod_rec, lw=1, c="k")
-ax[2].plot(time, beat, lw=1, c="k")
-ax[2].plot(
+plt.subplots_adjust(wspace=0.2)
+ax1.plot(time, eod_send, lw=1, c="gray")
+ax2.plot(time, eod_rec, lw=1, c="gray")
+ax3.plot(time, beat, lw=1, c="gray")
+ax3.plot(
     envelope_time,
     envelope,
-    lw=2,
-    color="red",
+    lw=3,
+    color=s.red,
 )
 
-ax[0].set_xlim(0.15, 0.25)
-ax[2].set_ylim(-2.1, 2.1)
+ax1.set_xlim(0, 0.3)
+ax2.set_ylim(-2.1, 2.1)
 
-ax[0].set_title(f"Sender: {eodf_send} Hz")
-ax[1].set_title(f"Receiver: {eodf_rec} Hz")
-ax[2].set_title(f"Amplitude modulation: {eodf_rec-eodf_send} Hz")
+ax1.text( 0.0, 0.99, f"Chirping sender: {eodf_send} Hz",transform=ax1.transAxes )
+ax2.text( 0.0, 0.99, f"Receiver: {eodf_rec} Hz", transform=ax2.transAxes)
+ax2.text(0.0, 0.99, f"Amplitude modulation: {eodf_rec-eodf_send} Hz", transform=ax3.transAxes)
 
 # remove axes
-for a in ax:
-    a.axis("off")
 
-plt.subplots_adjust(left=0.01, bottom=0.01, right=0.99, top=0.9, wspace=0, hspace=0.8)
+ax1.axis("off")
+
+ax2.axis("off")
+
+ax3.axis("off")
+
+
+plt.subplots_adjust(left=0.01, bottom=0.01, right=0.99, top=0.9, wspace=0.196, hspace=0.196)
 
 fs.doublesave("../figures/beat_without_chirp")
 plt.show()
